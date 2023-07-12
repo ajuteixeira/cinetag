f47af4dde5ea7deded691e31c930d733ac89ae24
Juliana Teixeira <teixeirajuuh@gmail.com>
2023-07-12 19:03:58 -0300

    title

---
+++ src/components/Title/index.js
@@ -0,0 +1,11 @@
+import styles from './Title.module.css'
+
+function Title({ children }) {
+  return (
+    <div className={styles.text}>
+      {children}
+    </div>
+  )
+}
+
+export default Title;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,16 @@
 import Banner from 'components/Banner';
 import Footer from 'components/Footer';
 import Header from 'components/Header'
+import Title from 'components/Title';
 
 function Home() {
   return (
     <>
       <Header />
       <Banner image="home" />
+      <Title>
+        <h1>A place to store your movies and videos!</h1>
+      </Title>
       <Footer />
     </>
   )
